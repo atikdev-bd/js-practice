4683bf1787e25b82347979329cc02f659de3056a
atikdev <atikdev.bdd@gamil.com>
2025-07-30 15:54:59 +0600

    for loop and while loop test

--- for-of.js
+++ for-of.js
@@ -1,10 +1,25 @@
 
 
-const numbers = [12, 76, 34, 47, 22, 14]
+// const numbers = [12, 76, 34, 47, 22, 14]
 
-console.log(numbers)
+// console.log(numbers)
 
 
-for (let num of numbers) {
-    console.log(num)
+// for (let num of numbers) {
+//     console.log(num)
+// }
+
+
+
+// for (i = 1; i <= 100; i++) {
+//     console.log(i + " " + 'i am sorry')
+// }
+
+
+let i = 1;
+
+while (i <= 40) {
+    console.log(i + 'Hello')
+
+    i++;
 }
